acea4f5d123e22925ef1e303ef96e618b8d84c7b
the-nazim <thenazim0707@gmail.com>
2025-10-22 13:09:09 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import Home from './pages/Home';
 import { Toaster } from './components/ui/toaster';
 import './App.css';
